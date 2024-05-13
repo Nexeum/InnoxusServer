a78559f2eeb88f54a02bb44c111caebc8b948e86
Nexeum <srandres1803@gmail.com>
2024-05-13 14:12:16 -0500

    fix: add return jwt

--- ms-auth/src/main/java/com/nexeum/msauth/domain/usecase/auth/AuthService.java
+++ ms-auth/src/main/java/com/nexeum/msauth/domain/usecase/auth/AuthService.java
@@ -1,15 +1,11 @@
 package com.nexeum.msauth.domain.usecase.auth;
 
-import com.mongodb.DuplicateKeyException;
-import com.mongodb.client.MongoClient;
 import com.nexeum.msauth.domain.model.auth.Auth;
 import com.nexeum.msauth.domain.usecase.auth.repository.AuthRepository;
 import com.nexeum.msauth.infrastructure.adapter.mongo.MongoAuthRepository;
-import com.nexeum.msauth.infrastructure.adapter.redis.RedisAuthRepository;
 import com.nexeum.msauth.infrastructure.helper.jwt.JwtGenerator;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import org.slf4j.Logger;
@@ -17,19 +13,14 @@ import org.slf4j.Logger;
 @Service
 public class AuthService implements AuthRepository {
     private final MongoAuthRepository mongoAuthRepository;
-    private final ReactiveCrudRepository<Auth, String> redisAuthRepository;
 
     private static final Logger log = LoggerFactory.getLogger(AuthService.class);
     private final JwtGenerator jwtGenerator;
-    private final MongoClient mongo;
-
 
     @Autowired
-    public AuthService(MongoAuthRepository mongoAuthRepository, ReactiveCrudRepository<Auth, String> redisAuthRepository, JwtGenerator jwtGenerator, MongoClient mongo) {
+    public AuthService(MongoAuthRepository mongoAuthRepository, JwtGenerator jwtGenerator) {
         this.mongoAuthRepository = mongoAuthRepository;
-        this.redisAuthRepository = redisAuthRepository;
         this.jwtGenerator = jwtGenerator;
-        this.mongo = mongo;
     }
 
     @Override
@@ -40,7 +31,7 @@ public class AuthService implements AuthRepository {
                     if (existingAuth.getPassword().equals(auth.getPassword())) {
                         log.info("User logged in");
                         String jwt = jwtGenerator.generateJwt(auth.getEmail());
-                        return redisAuthRepository.save(auth).thenReturn(jwt);
+                        return Mono.just(jwt);
                     } else {
                         log.error("Invalid password for email: {}", auth.getEmail());
                         return Mono.error(new IllegalArgumentException("Invalid password"));
@@ -61,14 +52,20 @@ public class AuthService implements AuthRepository {
                     return Mono.just("Email already exists");
                 })
                 .switchIfEmpty(
-                        mongoAuthRepository.save(auth)
-                                .doOnSuccess(result -> log.info("Registration successful for email: {}", auth.getEmail()))
-                                .doOnError(e -> log.error("Error during registration for email: {}", auth.getEmail(), e))
-                                .then(Mono.just("Registration successful for email: " + auth.getEmail()))
-                                .onErrorResume(e -> {
-                                    log.error("Error during registration for email: {}", auth.getEmail(), e);
-                                    return Mono.just("Error during registration: " + e.getMessage());
-                                })
-                );
+                    mongoAuthRepository.save(auth)
+                        .doOnSuccess(result -> log.info("Registration successful for email: {}", auth.getEmail()))
+                        .doOnError(e -> log.error("Error during registration for email: {}", auth.getEmail(), e))
+                        .then(Mono.just("Registration successful for email: " + auth.getEmail()))
+                )
+                .onErrorResume(e -> {
+                    log.error("Error during registration for email: {}", auth.getEmail(), e);
+                    return Mono.just("Error during registration: " + e.getMessage());
+                });
+    }
+
+    @Override
+    public Mono<Boolean> validateJwt(String token) {
+        return Mono.fromCallable(() -> jwtGenerator.validateJwt(token))
+                   .onErrorReturn(false);
     }
 }
