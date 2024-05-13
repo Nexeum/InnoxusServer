386a384b63eee6252b95dab5a37478f89b5a9e83
Nexeum <srandres1803@gmail.com>
2024-05-13 14:10:14 -0500

    feat: add validate token

--- ms-auth/src/main/java/com/nexeum/msauth/infrastructure/helper/jwt/JwtGenerator.java
+++ ms-auth/src/main/java/com/nexeum/msauth/infrastructure/helper/jwt/JwtGenerator.java
@@ -1,25 +1,32 @@
 package com.nexeum.msauth.infrastructure.helper.jwt;
 
+import com.nexeum.msauth.domain.usecase.auth.AuthService;
 import com.nexeum.msauth.infrastructure.helper.key.SecretKeyGenerator;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class JwtGenerator {
 
-    private final SecretKeyGenerator secretKeyGenerator;
+    private final SecretKey secretKey;
+
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
 
     public JwtGenerator() {
-        this.secretKeyGenerator = new SecretKeyGenerator();
+        this.secretKey = Keys.hmacShaKeyFor(new SecretKeyGenerator().generateSecretKey().getBytes());
     }
 
     public String generateJwt(String email) {
-        SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyGenerator.generateSecretKey().getBytes());
-
         long expirationTime = 1000 * 60 * 60 * 24;
 
         return Jwts.builder()
@@ -27,5 +34,22 @@ public class JwtGenerator {
                 .expiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(secretKey, Jwts.SIG.HS256)
                 .compact();
+    }
+
+    public boolean validateJwt(String token) {
+        log.info("Validating token");
+        try {
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+
+            log.info("Token expiration: {}", claimsJws.getPayload().getExpiration());
+            log.info(claimsJws.getPayload().getExpiration().after(new Date()) ? "Token is valid" : "Token is expired");
+            return claimsJws.getPayload().getExpiration().after(new Date());
+        } catch (JwtException e) {
+            log.error("Error validating token", e);
+            return false;
+        }
     }
 }
