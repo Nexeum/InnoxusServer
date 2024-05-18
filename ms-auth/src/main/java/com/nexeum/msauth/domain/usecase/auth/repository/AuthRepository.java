297a018d89209973fd9086400696285f5508b376
Nexeum <srandres1803@gmail.com>
2024-05-18 12:00:32 -0500

    fix: change params

--- ms-auth/src/main/java/com/nexeum/msauth/domain/usecase/auth/repository/AuthRepository.java
+++ ms-auth/src/main/java/com/nexeum/msauth/domain/usecase/auth/repository/AuthRepository.java
@@ -1,10 +1,13 @@
 package com.nexeum.msauth.domain.usecase.auth.repository;
 
 import com.nexeum.msauth.domain.model.auth.Auth;
+import com.nexeum.msauth.domain.model.jwt.Token;
+
 import reactor.core.publisher.Mono;
 
 public interface AuthRepository {
     Mono<String> login(Auth auth);
     Mono<String> register(Auth auth);
-    Mono<Boolean> validateJwt(String token);
+    Mono<Boolean> validateJwt(Token token);
+    Mono<String> getEmailFromJwt(Token token);
 }
