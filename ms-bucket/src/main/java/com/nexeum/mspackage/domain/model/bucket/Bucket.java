afb4e6b03228e32dbb1feb2f11a45334052ecb6e
Nexeum <srandres1803@gmail.com>
2024-05-19 17:17:44 -0500

    feat: add stars and branches

--- ms-bucket/src/main/java/com/nexeum/mspackage/domain/model/bucket/Bucket.java
+++ ms-bucket/src/main/java/com/nexeum/mspackage/domain/model/bucket/Bucket.java
@@ -4,12 +4,13 @@ import org.springframework.data.mongodb.core.index.Indexed;
 
 import java.util.List;
 
-
 @Data
 public class Bucket {
     @Indexed(unique = true)
     private String id;
     private String name;
+    private int stars;
+    private List<String> branches;
     private String description;
     private List<String> files;
     private String[] languages;
