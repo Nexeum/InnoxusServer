b5a9f87025f005d86a73c6d4ed8a4f3436d86a0e
Nexeum <srandres1803@gmail.com>
2024-05-24 15:29:09 -0500

    feat: add TypeAlias Mongo

--- ms-workspace/src/main/java/com/nexeum/msworkspace/domain/model/workspace/Workspace.java
+++ ms-workspace/src/main/java/com/nexeum/msworkspace/domain/model/workspace/Workspace.java
@@ -1,9 +1,14 @@
 package com.nexeum.msworkspace.domain.model.workspace;
 
 import lombok.Data;
+
+import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document
+@TypeAlias("workspace")
 public class Workspace {
     @Indexed(unique = true)
     private String id;
