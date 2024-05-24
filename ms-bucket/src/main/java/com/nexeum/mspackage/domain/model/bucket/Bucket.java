d413d13ab54a5e0c90f8d26b238d4598719eb1d7
Nexeum <srandres1803@gmail.com>
2024-05-24 15:29:36 -0500

    feat: add TypeAlias Mongo

--- ms-bucket/src/main/java/com/nexeum/mspackage/domain/model/bucket/Bucket.java
+++ ms-bucket/src/main/java/com/nexeum/mspackage/domain/model/bucket/Bucket.java
@@ -1,10 +1,15 @@
 package com.nexeum.mspackage.domain.model.bucket;
 import lombok.Data;
+
+import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Data
+@Document
+@TypeAlias("bucket")
 public class Bucket {
     @Indexed(unique = true)
     private String id;
